8a961bf044e16c6387d34870626a13dcf2400f84
Nikolay Aleksandrov <nikolay@cumulusnetworks.com>
2020-01-14 19:56:09 +0200

    net: bridge: vlan: add rtm definitions and dump support
    This patch adds vlan rtm definitions:
     - NEWVLAN: to be used for creating vlans, setting options and
       notifications
     - DELVLAN: to be used for deleting vlans
     - GETVLAN: used for dumping vlan information
    
    Dumping vlans which can span multiple messages is added now with basic
    information (vid and flags). We use nlmsg_parse() to validate the header
    length in order to be able to extend the message with filtering
    attributes later.
    
    Signed-off-by: Nikolay Aleksandrov <nikolay@cumulusnetworks.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 8dcea187088bce5d2f1149294ad109f022653547)
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

--- net/bridge/br_netlink.c
+++ net/bridge/br_netlink.c
@@ -1661,6 +1661,7 @@ int __init br_netlink_init(void)
 	int err;
 
 	br_mdb_init();
+	br_vlan_rtnl_init();
 	rtnl_af_register(&br_af_ops);
 
 	err = rtnl_link_register(&br_link_ops);
@@ -1678,6 +1679,7 @@ out_af:
 void br_netlink_fini(void)
 {
 	br_mdb_uninit();
+	br_vlan_rtnl_uninit();
 	rtnl_af_unregister(&br_af_ops);
 	rtnl_link_unregister(&br_link_ops);
 }
