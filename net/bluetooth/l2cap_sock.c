a61b4a7b0fe767a635330aa253c5c6f0ee627fa8
Gustavo A. R. Silva <gustavoars@kernel.org>
2020-07-08 15:18:23 -0500

    Bluetooth: core: Use fallthrough pseudo-keyword
    Replace the existing /* fall through */ comments and its variants with
    the new pseudo-keyword macro fallthrough[1]. Also, remove unnecessary
    fall-through markings when it is the case.
    
    [1] https://www.kernel.org/doc/html/latest/process/deprecated.html?highlight=fallthrough#implicit-switch-case-fall-through
    
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    
    (cherry picked from commit 19186c7b45c134820ea6fde3165a2cf30c1ace47)
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

--- net/bluetooth/l2cap_sock.c
+++ net/bluetooth/l2cap_sock.c
@@ -287,7 +287,7 @@ static int l2cap_sock_listen(struct socket *sock, int backlog)
 	case L2CAP_MODE_STREAMING:
 		if (!disable_ertm)
 			break;
-		/* fall through */
+		fallthrough;
 	default:
 		err = -EOPNOTSUPP;
 		goto done;
@@ -710,7 +710,7 @@ static int l2cap_sock_setsockopt_old(struct socket *sock, int optname,
 		case L2CAP_MODE_STREAMING:
 			if (!disable_ertm)
 				break;
-			/* fall through */
+			fallthrough;
 		default:
 			err = -EINVAL;
 			break;
