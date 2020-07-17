58b9debdba189eb6d8789c746ac36ffdda3e3e9d
Christoph Hellwig <hch@lst.de>
2020-07-17 08:23:26 +0200

    net/ipv4: remove compat_ip_{get,set}sockopt
    Handle the few cases that need special treatment in-line using
    in_compat_syscall().
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit b6238c04c0e5dbe7ae4ea48e96e004905b120a04)
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

--- net/ipv4/udplite.c
+++ net/ipv4/udplite.c
@@ -66,10 +66,6 @@ struct proto 	udplite_prot = {
 	.sysctl_rmem_offset = offsetof(struct net, ipv4.sysctl_udp_rmem_min),
 	.obj_size	   = sizeof(struct udp_sock),
 	.h.udp_table	   = &udplite_table,
-#ifdef CONFIG_COMPAT
-	.compat_setsockopt = compat_udp_setsockopt,
-	.compat_getsockopt = compat_udp_getsockopt,
-#endif
 };
 EXPORT_SYMBOL(udplite_prot);
 
