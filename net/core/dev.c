838523220afba495e0506f9f16c396102e1c96e0
Jakub Kicinski <kuba@kernel.org>
2020-09-09 10:37:52 -0700

    net: manage napi add/del idempotence explicitly
    To RCUify napi->dev_list we need to replace list_del_init()
    with list_del_rcu(). There is no _init() version for RCU for
    obvious reasons. Up until now netif_napi_del() was idempotent
    so to make sure it remains such add a bit which is set when
    NAPI is listed, and cleared when it removed. Since we don't
    expect multiple calls to netif_napi_add() to be correct,
    add a warning on that side.
    
    Now that napi_hash_add / napi_hash_del are only called by
    napi_add / del we can actually steal its bit. We just need
    to make sure hash node is initialized correctly.
    
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 4d092dd2041a5f328410ad16d63b8606c24e8604)
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -6732,8 +6732,7 @@ EXPORT_SYMBOL(napi_busy_loop);
 
 static void napi_hash_add(struct napi_struct *napi)
 {
-	if (test_bit(NAPI_STATE_NO_BUSY_POLL, &napi->state) ||
-	    test_and_set_bit(NAPI_STATE_HASHED, &napi->state))
+	if (test_bit(NAPI_STATE_NO_BUSY_POLL, &napi->state))
 		return;
 
 	spin_lock(&napi_hash_lock);
@@ -6758,8 +6757,7 @@ static void napi_hash_del(struct napi_struct *napi)
 {
 	spin_lock(&napi_hash_lock);
 
-	if (test_and_clear_bit(NAPI_STATE_HASHED, &napi->state))
-		hlist_del_rcu(&napi->napi_hash_node);
+	hlist_del_init_rcu(&napi->napi_hash_node);
 
 	spin_unlock(&napi_hash_lock);
 }
@@ -6837,7 +6835,11 @@ int dev_set_threaded(struct net_device *dev, bool threaded)
 void netif_napi_add(struct net_device *dev, struct napi_struct *napi,
 		    int (*poll)(struct napi_struct *, int), int weight)
 {
+	if (WARN_ON(test_and_set_bit(NAPI_STATE_LISTED, &napi->state)))
+		return;
+
 	INIT_LIST_HEAD(&napi->poll_list);
+	INIT_HLIST_NODE(&napi->napi_hash_node);
 	hrtimer_init(&napi->timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL_PINNED);
 	napi->timer.function = napi_watchdog;
 	init_gro_hash(napi);
@@ -6917,6 +6919,9 @@ static void flush_gro_hash(struct napi_struct *napi)
 /* Must be called in process context */
 void __netif_napi_del(struct napi_struct *napi)
 {
+	if (!test_and_clear_bit(NAPI_STATE_LISTED, &napi->state))
+		return;
+
 	napi_hash_del(napi);
 	list_del_init(&napi->dev_list);
 	napi_free_frags(napi);
