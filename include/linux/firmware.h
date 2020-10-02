889a60d84a1b38685d5d8792f35f709b150c0a37
Scott Branden <scott.branden@broadcom.com>
2020-10-02 10:38:27 -0700

    firmware: Add request_partial_firmware_into_buf()
    Add request_partial_firmware_into_buf() to allow for portions of a
    firmware file to be read into a buffer. This is needed when large firmware
    must be loaded in portions from a file on memory constrained systems.
    
    Signed-off-by: Scott Branden <scott.branden@broadcom.com>
    Co-developed-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Link: https://lore.kernel.org/r/20201002173828.2099543-16-keescook@chromium.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 59cdb23ca2dfef3b93411d1105409dfe9cd1f62f)
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

--- include/linux/firmware.h
+++ include/linux/firmware.h
@@ -53,6 +53,9 @@ int request_firmware_direct(const struct firmware **fw, const char *name,
 			    struct device *device);
 int request_firmware_into_buf(const struct firmware **firmware_p,
 	const char *name, struct device *device, void *buf, size_t size);
+int request_partial_firmware_into_buf(const struct firmware **firmware_p,
+				      const char *name, struct device *device,
+				      void *buf, size_t size, size_t offset);
 
 void release_firmware(const struct firmware *fw);
 #if IS_ENABLED(CONFIG_OPLUS_FEATURE_WIFI_BDF)
@@ -103,6 +106,15 @@ static inline int request_firmware_direct(const struct firmware **fw,
 
 static inline int request_firmware_into_buf(const struct firmware **firmware_p,
 	const char *name, struct device *device, void *buf, size_t size)
+{
+	return -EINVAL;
+}
+
+static inline int request_partial_firmware_into_buf
+					(const struct firmware **firmware_p,
+					 const char *name,
+					 struct device *device,
+					 void *buf, size_t size, size_t offset)
 {
 	return -EINVAL;
 }
