55004364fe1458e0c1634a5507beed060833a381
Paul E. McKenney <paulmck@kernel.org>
2021-03-25 19:39:14 -0700

    torture: Make kvm-find-errors.sh account for kvm-remote.sh
    Currently, kvm-find-errors.sh assumes that if "--buildonly" appears in
    the log file, then the run did builds but ran no kernels.  This breaks
    with kvm-remote.sh, which uses kvm.sh to do a build, then kvm-again.sh
    to run the kernels built on remote systems.  This commit therefore adds
    a check for a kvm-remote.sh run.
    
    While in the area, this commit checks for "--build-only" as well as
    "--build-only".
    
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    (cherry picked from commit e1f2a28c2b946c7d558bb12ecdf2a40c41a5b375)
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

--- tools/testing/selftests/rcutorture/bin/kvm-find-errors.sh
+++ tools/testing/selftests/rcutorture/bin/kvm-find-errors.sh
@@ -41,7 +41,7 @@ then
 else
 	echo No build errors.
 fi
-if grep -q -e "--buildonly" < ${rundir}/log
+if grep -q -e "--build-\?only" < ${rundir}/log && ! test -f "${rundir}/remote-log"
 then
 	echo Build-only run, no console logs to check.
 fi
