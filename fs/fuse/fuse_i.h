382c7e971a80bbd3e529c1ef3321c3ff6fbf1aa9
Paul Lawrence <paullawrence@google.com>
2022-03-04 20:11:59 +0000

    ANDROID: fuse-bpf: Move fd operations to be synchronous
    Bug: 222619123
    Test: fuse_test passes, on cuttlefish CtsCameraTestCases passes
    Signed-off-by: Paul Lawrence <paullawrence@google.com>
    Change-Id: I54c148206b5ad5ae5737939bcb076cbe6c40129c
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    (cherry picked from commit abb9a4747b5f2715f51b1922d50b9fb859d7fede)
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -860,9 +860,6 @@ struct fuse_conn {
 		/* Request timeout (in jiffies). 0 = no timeout */
 		unsigned int req_timeout;
 	} timeout;
-
-	/** task_struct for fd lookups in fuse-bpf */
-	struct task_struct *task;
 };
 
 /*
@@ -1326,8 +1323,7 @@ void fuse_dax_cancel_work(struct fuse_conn *fc);
 
 /* backing.c */
 
-struct file *fuse_fget(struct fuse_conn *fc, unsigned int fd);
-struct bpf_prog *fuse_get_bpf_prog(struct fuse_conn *fc, unsigned int fd);
+struct bpf_prog *fuse_get_bpf_prog(struct file *file);
 
 /*
  * Dummy io passed to fuse_bpf_backing when io operation needs no scratch space
