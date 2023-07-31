e598c6d8cf5de08b43cf9ff1907c915928473f20
EndCredits <endcredits@crepuscular-aosp.icu>
2023-07-31 23:21:23 +0800

    msm: video: Compile out debugfs related code
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>
    (cherry picked from commit 1677afec20bd4f6b011eba76d2d43d4599eb92ec)
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>
    (cherry picked from commit 20f1ca696a7556293f76396b6fce542ec9a74407)
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

--- techpack/video/msm/vidc/msm_vidc.c
+++ techpack/video/msm/vidc/msm_vidc.c
@@ -1620,10 +1620,10 @@ void *msm_vidc_open(int core_id, int session_type)
 	}
 
 	msm_comm_scale_clocks_and_bus(inst, 1);
-
+#ifdef CONFIG_DEBUG_FS
 	inst->debugfs_root =
 		msm_vidc_debugfs_init_inst(inst, core->debugfs_root);
-
+#endif
 	return inst;
 fail_bufq_output:
 	vb2_queue_release(&inst->bufq[OUTPUT_PORT].vb2_bufq);
@@ -1781,7 +1781,9 @@ int msm_vidc_destroy(struct msm_vidc_inst *inst)
 	mutex_destroy(&inst->bufq[INPUT_PORT].lock);
 	mutex_destroy(&inst->lock);
 
+#ifdef CONFIG_DEBUG_FS
 	msm_vidc_debugfs_deinit_inst(inst);
+#endif
 
 	pr_debug(VIDC_DBG_TAG "Closed video instance: %pK\n",
 			"high", inst->sid, get_codec_name(inst->sid),
--- techpack/video/msm/vidc/msm_vidc_debug.c
+++ techpack/video/msm/vidc/msm_vidc_debug.c
@@ -204,6 +204,7 @@ static const struct file_operations debug_level_fops = {
 	.read = debug_level_read,
 };
 
+#ifdef CONFIG_DEBUG_FS
 struct dentry *msm_vidc_debugfs_init_drv(void)
 {
 	bool ok = false;
@@ -293,6 +294,7 @@ struct dentry *msm_vidc_debugfs_init_core(struct msm_vidc_core *core,
 failed_create_dir:
 	return dir;
 }
+#endif
 
 static int inst_info_open(struct inode *inode, struct file *file)
 {
@@ -469,6 +471,7 @@ static const struct file_operations inst_info_fops = {
 	.release = inst_info_release,
 };
 
+#ifdef CONFIG_DEBUG_FS
 struct dentry *msm_vidc_debugfs_init_inst(struct msm_vidc_inst *inst,
 		struct dentry *parent)
 {
@@ -594,6 +597,7 @@ void msm_vidc_debugfs_update(struct msm_vidc_inst *inst,
 		break;
 	}
 }
+#endif
 
 int msm_vidc_check_ratelimit(void)
 {
