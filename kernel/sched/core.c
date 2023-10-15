7481b388b5164503700745cd27eac2c8c775c132
Alexander Winkowski <dereference23@outlook.com>
2023-10-15 09:12:12 +0000

    Revert "sched: Add sched_enq_deq_task trace point"
    This reverts commit 8573d7bfaa61f080a5b632897fd4457a0873300b.
    
    Change-Id: If559d3577bc2cb3591e699548170bca71d908b19
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    (cherry picked from commit 3ea32cd14a26ae0e5615e7ce082a395faed7c6a7)
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1410,7 +1410,6 @@ static inline void enqueue_task(struct rq *rq, struct task_struct *p, int flags)
 	uclamp_rq_inc(rq, p);
 	p->sched_class->enqueue_task(rq, p, flags);
 	walt_update_last_enqueue(p);
-	trace_sched_enq_deq_task(p, 1, cpumask_bits(&p->cpus_mask)[0]);
 
 	trace_android_rvh_enqueue_task(rq, p);
 }
@@ -1431,7 +1430,6 @@ static inline void dequeue_task(struct rq *rq, struct task_struct *p, int flags)
 	if (p == rq->wrq.ed_task)
 		early_detection_notify(rq, sched_ktime_clock());
 #endif
-	trace_sched_enq_deq_task(p, 0, cpumask_bits(&p->cpus_mask)[0]);
 
 	trace_android_rvh_dequeue_task(rq, p);
 }
