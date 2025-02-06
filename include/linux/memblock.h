e288f9acf909911af86e003d44925c51148e7a71
Daniel Micay <daniel.micay@grapheneos.org>
2025-02-06 07:32:14 -0500

    zero memory in early boot

--- include/linux/memblock.h
+++ include/linux/memblock.h
@@ -573,6 +573,8 @@ static inline void early_memtest(phys_addr_t start, phys_addr_t end)
 }
 #endif
 
+extern void early_memzero(phys_addr_t start, phys_addr_t end);
+
 #endif /* __KERNEL__ */
 
 #endif /* _LINUX_MEMBLOCK_H */
