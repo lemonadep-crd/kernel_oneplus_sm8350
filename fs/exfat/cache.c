ffe42bc4e52898c7b5f9aafc662ce3f60c089b4b
Chi Zhiling <chizhiling@kylinos.cn>
2026-01-14 20:12:44 +0800

    exfat: reuse cache to improve exfat_get_cluster
    Since exfat_ent_get supports cache buffer head, we can use this option to
    reduce sb_bread calls when fetching consecutive entries.
    
    Signed-off-by: Chi Zhiling <chizhiling@kylinos.cn>
    Reviewed-by: Yuezhang Mo <Yuezhang.Mo@sony.com>
    Signed-off-by: Namjae Jeon <linkinjeon@kernel.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    (cherry picked from commit bcc04b505e5d97a3cd02d3f5dcfd4d266cb7e8da)
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

--- fs/exfat/cache.c
+++ fs/exfat/cache.c
@@ -242,6 +242,7 @@ int exfat_get_cluster(struct inode *inode, unsigned int cluster,
 {
 	struct super_block *sb = inode->i_sb;
 	struct exfat_inode_info *ei = EXFAT_I(inode);
+	struct buffer_head *bh = NULL;
 	struct exfat_cache_id cid;
 	unsigned int content, fclus;
 
@@ -269,7 +270,7 @@ int exfat_get_cluster(struct inode *inode, unsigned int cluster,
 		return 0;
 
 	while (fclus < cluster) {
-		if (exfat_ent_get(sb, *dclus, &content, NULL))
+		if (exfat_ent_get(sb, *dclus, &content, &bh))
 			return -EIO;
 
 		*last_dclus = *dclus;
@@ -283,6 +284,7 @@ int exfat_get_cluster(struct inode *inode, unsigned int cluster,
 			cache_init(&cid, fclus, *dclus);
 	}
 
+	brelse(bh);
 	exfat_cache_add(inode, &cid);
 	return 0;
 }
