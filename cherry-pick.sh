2ddcb693119c46d36ca66a47fe1e137abce74f3b
Madara273 <ravenhoxs@gmail.com>
2026-01-25 10:11:54 +0000

    cherry-pick: Improve commit signing script
    Change-Id: Ie0139da03e0305158369c1ff2f253c20eaa2a11b
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

--- cherry-pick.sh
+++ cherry-pick.sh
@@ -1,29 +1,61 @@
 #!/bin/bash
 #
 # EXAMPLES
-#   ./cherry-pick.sh <commit-hash>
+#	./cherry-pick.sh <commit-hash>
 #
 
-# Cherry-pick provided commit with signature
-git cherry-pick "$@" -x -s -m 1
-
-# Verify if cherry-pick was successful
-if [ $? -eq 0 ]; then 
-
-    # Add a Signed-off-by line in the commit message, if not present
-    commit_file=$(git rev-parse --git-dir)/COMMIT_EDITMSG
-    if ! grep -qs "^Signed-off-by:" "$commit_file"; then
-        echo -e "\nSigned-off-by: TogoFire <togofire@mailfence.com>\n" >> "$commit_file"
-    fi
-
-    # Show a success message and exit
-    echo "Cherry-pick successful!"
-    exit 0
-
-else 
-
-    # Show an error message and exit
-    echo "Cherry-pick failed"
-    exit 1
-
+if [ $# -eq 0 ]; then
+	echo "Usage: $0 <commit-hash>"
+	exit 1
 fi
+
+commit_hash="$1"
+
+git cherry-pick "$@" -x -s -m 1
+rc=$?
+
+if [ $rc -ne 0 ]; then
+	echo "Cherry-pick failed"
+	exit 1
+fi
+
+git_name=$(git config user.name)
+git_email=$(git config user.email)
+
+if [ -z "$git_name" ] || [ -z "$git_email" ]; then
+	echo "Error: git user.name or user.email is not set"
+	exit 1
+fi
+
+target_signature="Signed-off-by: $git_name <$git_email>"
+tmp=$(mktemp)
+
+# clean commit message
+while IFS= read -r line; do
+	# remove duplicate Signed-off-by
+	if [[ "$line" == "$target_signature" ]]; then
+		continue
+	fi
+
+	# remove ALL cherry-pick markers
+	if [[ "$line" =~ ^\(cherry\ picked\ from\ commit\ .* ]]; then
+		continue
+	fi
+
+	echo "$line"
+done <<< "$(git log -1 --pretty=%B)" > "$tmp"
+
+# normalize + add ONE correct footer
+{
+	git stripspace < "$tmp"
+	echo
+	echo "(cherry picked from commit $commit_hash)"
+	echo "$target_signature"
+} > "${tmp}.final"
+
+git commit --amend --no-edit -F "${tmp}.final"
+
+rm -f "$tmp" "${tmp}.final"
+
+echo "Cherry-pick successful! Message cleaned."
+exit 0
