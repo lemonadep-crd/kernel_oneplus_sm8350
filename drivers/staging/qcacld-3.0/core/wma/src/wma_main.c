Spark-Devices/android_kernel_xiaomi_mojito

qcacld-3.0: Drop mac_phy_cap evaulation as true
- This fixes the following warning:

/home/matheus/derp/kernel/oneplus/sm8150/drivers/staging/qcacld-3.0/core/wma/src/wma_main.c: In function 'wma_print_populate_soc_caps':
  CC      drivers/staging/qcacld-3.0/core/dp/txrx/ol_txrx_ipa.o
/home/matheus/derp/kernel/oneplus/sm8150/drivers/staging/qcacld-3.0/core/wma/src/wma_main.c:6315:21: error: the comparison will always evaluate as 'true' for the pointer operand in 'mac_phy_cap + (sizetype)((long unsigned int)i * 244)' must not be NULL [-Werror=address]
 6315 |                 if (&mac_phy_cap[i]) {
      |

Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

drivers/staging/qcacld-3.0/core/wma/src/wma_main.c
@@ -6312,13 +6312,11 @@ static void wma_print_populate_soc_caps(struct target_psoc_info *tgt_hdl)
 	WMA_LOGD("%s: <====== HW mode cap printing starts ======>", __func__);
 	/* print cap of each hw mode */
 	for (i = 0; i < total_mac_phy_cnt; i++) {
-		if (&mac_phy_cap[i]) {
-			WMA_LOGD("====>: hw mode id[%d], phy id[%d]",
-				 mac_phy_cap[i].hw_mode_id,
-				 mac_phy_cap[i].phy_id);
-			tmp = &mac_phy_cap[i];
-			wma_print_mac_phy_capabilities(tmp, i);
-		}
+	  WMA_LOGD("====>: hw mode id[%d], phy id[%d]",
+		   mac_phy_cap[i].hw_mode_id,
+		   mac_phy_cap[i].phy_id);
+	  tmp = &mac_phy_cap[i];
+	  wma_print_mac_phy_capabilities(tmp, i);
 	}
 	WMA_LOGD("%s: <====== HW mode cap printing ends ======>\n", __func__);
 }

lemonadep-crd/kernel_oneplus_sm8350

Change-Id: Ifb64af14f7bd6215682c5fb73e77ead250890337
Signed-off-by: TogoFire <togofire@mailfence.com>
(cherry picked from commit 2b21f302245bdaeb0d5bb591bf24f2ad00b47bf7)
Signed-off-by: Madara273 <ravenhoxs@gmail.com>

drivers/staging/qcacld-3.0/core/wma/src/wma_main.c
@@ -6540,13 +6540,11 @@ static void wma_print_populate_soc_caps(struct target_psoc_info *tgt_hdl)
 	wma_debug("<====== HW mode cap printing starts ======>");
 	/* print cap of each hw mode */
 	for (i = 0; i < total_mac_phy_cnt; i++) {
-		if (&mac_phy_cap[i]) {
-			wma_nofl_debug("====>: hw mode id[%d], phy id[%d]",
-				 mac_phy_cap[i].hw_mode_id,
-				 mac_phy_cap[i].phy_id);
-			tmp = &mac_phy_cap[i];
-			wma_print_mac_phy_capabilities(tmp, i);
-		}
+		wma_nofl_debug("====>: hw mode id[%d], phy id[%d]",
+			 mac_phy_cap[i].hw_mode_id,
+			 mac_phy_cap[i].phy_id);
+		tmp = &mac_phy_cap[i];
+		wma_print_mac_phy_capabilities(tmp, i);
 	}
 	wma_debug("<====== HW mode cap printing ends ======>\n");
 }